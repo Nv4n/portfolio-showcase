008e62ea26cf6e571a5a683a6a209191b5d3f595
Sybatron <vipsister2014@gmail.com>
2026-04-30 15:13:45 +0300

    update reset-password page and form

---
+++ app/gfe/reset-password/page.tsx
@@ -0,0 +1,26 @@
+import { PasswordResetForm } from "@/app/gfe/reset-password/password-reset-form";
+import {
+	Card,
+	CardHeader,
+	CardTitle,
+	CardDescription,
+	CardContent,
+} from "@/components/ui/card";
+
+export default function ResetPassword() {
+	return (
+		<Card className="w-full max-w-4xl gap-8">
+			<CardHeader>
+				<CardTitle className="text-xl font-semibold">
+					Manage Security
+				</CardTitle>
+				<CardDescription>
+					Protect your data and ensure secure interactions.
+				</CardDescription>
+			</CardHeader>
+			<CardContent>
+				<PasswordResetForm />
+			</CardContent>
+		</Card>
+	);
+}
---
+++ app/gfe/reset-password/password-reset-form.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { PasswordField } from "@/components/password-field";
+import { RequirementList } from "@/components/requirements-list";
+import { Button } from "@/components/ui/button";
+import { Field, FieldGroup } from "@/components/ui/field";
+import { ResetPass, ResetPassSchema } from "@/types/reset-password";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+export function PasswordResetForm() {
+	const form = useForm<ResetPass>({
+		resolver: zodResolver(ResetPassSchema),
+		defaultValues: {
+			currentPassword: "",
+			newPassword: "",
+			confirmPassword: "",
+		},
+		mode: "onSubmit",
+	});
+
+	function onSubmit(data: ResetPass) {
+		console.log(data);
+	}
+
+	const passwordValue = form.watch("newPassword");
+
+	return (
+		<form id="account-rhf" onSubmit={form.handleSubmit(onSubmit)}>
+			<FieldGroup className="mb-8 gap-8 sm:gap-6">
+				<PasswordField
+					rhfForm={form}
+					name={"currentPassword"}
+					labelText={"Current password"}
+					placeholder="Enter your current password"
+				></PasswordField>
+				<PasswordField
+					rhfForm={form}
+					name={"newPassword"}
+					labelText={"New password"}
+					placeholder="Enter your new password"
+				></PasswordField>
+				<RequirementList password={passwordValue} />
+				<PasswordField
+					rhfForm={form}
+					name={"confirmPassword"}
+					labelText={"Confirm new password"}
+					placeholder="Repeat your new password"
+				></PasswordField>
+			</FieldGroup>
+			<Field orientation="horizontal">
+				<Button
+					disabled={
+						!form.formState.isDirty || !form.formState.isValid
+							? true
+							: false
+					}
+					type="submit"
+					className="text-background ml-auto w-44 cursor-pointer rounded bg-indigo-700 px-3 py-5 font-medium hover:bg-indigo-800 focus:bg-indigo-800 disabled:bg-neutral-100 disabled:text-neutral-400"
+				>
+					Save changes
+				</Button>
+			</Field>
+		</form>
+	);
+}
