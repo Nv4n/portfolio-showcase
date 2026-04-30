c428f8eedc0867af69e31fa8673317cc69a2a86a
Sybatron <vipsister2014@gmail.com>
2026-04-30 15:15:49 +0300

    update tos zod schema

--- types/form-auth.ts
+++ types/form-auth.ts
@@ -1,6 +1,5 @@
+import { PASSWORD_ZOD } from "@/types/password";
 import z from "zod";
-
-const PASS_ERROR_PREFIX = "Password should contain";
 
 export const AuthSignInSchema = z.object({
 	email: z
@@ -13,26 +12,10 @@ export const AuthSignUpSchema = z.object({
 	email: z
 		.email("Please enter a valid email address.")
 		.nonempty("Email address is required."),
-	password: z
-		.string()
-		.trim()
-		.nonempty("Password is required.")
-		.min(8, `${PASS_ERROR_PREFIX} 8 - 64 characters.`)
-		.max(64, `${PASS_ERROR_PREFIX} 8 - 64 characters.`)
-		.regex(
-			/^[a-z]+$/,
-			`${PASS_ERROR_PREFIX} at least one uppercase letter.`,
-		)
-		.regex(
-			/^[A-Z]+$/,
-			`${PASS_ERROR_PREFIX} at least one lowercase letter.`,
-		)
-		.regex(/^[0-9]+$/, `${PASS_ERROR_PREFIX} at least one number.`)
-		.regex(
-			/^[!@#$%^&*]+$/,
-			`${PASS_ERROR_PREFIX} at least one special character (e.g., ! @ # $ % ^ & *).`,
-		),
-	tos: z.boolean(),
+	password: PASSWORD_ZOD,
+	tos: z
+		.boolean()
+		.refine((val) => val, "You must accept the terms and conditions."),
 });
 
 export type AuthSignIn = z.infer<typeof AuthSignInSchema>;
