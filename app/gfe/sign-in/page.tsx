adfcc3096f09a99784a8dc97c8ba2dfbb50a7a30
Sybatron <vipsister2014@gmail.com>
2026-04-30 13:09:20 +0300

    add sign in page and form

---
+++ app/gfe/sign-in/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { SignInForm } from "@/app/gfe/sign-in/sign-in-form";
+import { Card, CardHeader, CardTitle, CardContent } from "@/components/ui/card";
+
+export default function SignIn() {
+	return (
+		<Card>
+			<CardHeader>
+				<CardTitle>Log in to your account</CardTitle>
+			</CardHeader>
+			<CardContent>
+				<SignInForm></SignInForm>
+			</CardContent>
+		</Card>
+	);
+}
---
+++ app/gfe/sign-in/sign-in-form.tsx
@@ -0,0 +1,62 @@
+
+
+import { PasswordInput } from "@/components/password-input";
+import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
+import { Input } from "@/components/ui/input";
+import { AuthSignInSchema, type AuthSignIn } from "@/types/form-auth";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+
+export function SignInForm() {
+	const form = useForm<AuthSignIn>({
+		resolver: zodResolver(AuthSignInSchema),
+		defaultValues: {
+			email: "",
+			password: "",
+		},
+	});
+	const password = form.watch("password");
+
+	function onSubmit(data: AuthSignIn) {
+		// const fetch
+		console.log(data);
+	}
+
+	return (
+		<form onSubmit={form.handleSubmit(onSubmit)}>
+			<FieldGroup>
+				<Controller
+					name="email"
+					control={form.control}
+					render={({ field, fieldState }) => (
+						<Field>
+							<FieldLabel htmlFor="email">Email</FieldLabel>
+							<Input
+								{...field}
+								id="email"
+								aria-invalid={fieldState.invalid}
+								placeholder="john@example.com"
+								autoComplete="email"
+							></Input>
+						</Field>
+					)}
+				></Controller>
+				<Controller
+					name="password"
+					control={form.control}
+					render={({ field, fieldState }) => (
+						<Field>
+							<FieldLabel htmlFor="password">Password</FieldLabel>
+							<PasswordInput
+								field={field}
+								fieldState={fieldState}
+								id="password"
+								value={password}
+							/>
+						</Field>
+					)}
+				></Controller>
+			</FieldGroup>
+		</form>
+	);
+}
