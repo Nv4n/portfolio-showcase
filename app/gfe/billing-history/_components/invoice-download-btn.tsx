7fbb14c8361e53f0e45c6f4fd916540ddb4e7238
Sybatron <vipsister2014@gmail.com>
2026-06-22 00:53:28 +0300

    fix download button
     downloads only invoice-0 for now

--- app/gfe/billing-history/_components/invoice-download-btn.tsx
+++ app/gfe/billing-history/_components/invoice-download-btn.tsx
@@ -1,22 +1,72 @@
-import { buttonVariants } from '@/components/ui/button';
-import { cn } from '@/lib/utils';
-import Link from 'next/link';
+import { gfeToast } from "@/components/toast-notification";
+import { Button, buttonVariants } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { useQuery } from "@tanstack/react-query";
+import Link from "next/link";
 
 interface InvoiceDownloadProps {
 	invoiceId: number;
 }
 
 export function InvoiceDownloadBtn({ invoiceId }: InvoiceDownloadProps) {
+	const { data, isPending, isFetching, isError, refetch } = useQuery({
+		queryKey: ["invoice", invoiceId],
+		queryFn: async () => {
+			const res = await fetch(`/api/invoice/pdf/${invoiceId}`);
+			if (!res.ok) {
+				throw new Error("Network response was not ok");
+			}
+			const json = await res.json();
+			return json.url;
+		},
+		retry: false,
+	});
+
+	if (isError) {
+		gfeToast({
+			variant: "error",
+			description: "Error downloading invoice",
+		});
+		return (
+			<Button
+				className={cn(
+					buttonVariants({
+						variant: "link",
+					}),
+					"ml-auto block w-fit h-fit text-indigo-700 hover:text-indigo-800",
+				)}
+				onClick={() => refetch()}
+				disabled={isFetching}
+			>
+				Try again
+			</Button>
+		);
+	}
+	if (isPending || isFetching) {
+		return (
+			<p
+				className={cn(
+					buttonVariants({
+						variant: "link",
+					}),
+					"ml-auto block w-fit h-fit text-indigo-700 hover:text-indigo-800",
+				)}
+			>
+				Loading ...
+			</p>
+		);
+	}
+
 	return (
 		<Link
-			href={`/api/invoice/pdf/${invoiceId}`}
+			href={data}
 			target="_blank"
 			rel="noopener noreferrer"
 			className={cn(
 				buttonVariants({
-					variant: 'link',
+					variant: "link",
 				}),
-				'ml-auto block w-fit h-fit text-indigo-700 hover:text-indigo-800',
+				"ml-auto block w-fit h-fit text-indigo-700 hover:text-indigo-800",
 			)}
 		>
 			Download
